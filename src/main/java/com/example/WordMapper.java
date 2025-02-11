b158526dcf9d29074066800db873a81590b3481c
vamsikopparthi24 <vamsikopparthi24@gmail.com>
2025-02-11 19:12:21 +0000

    Successful

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,27 @@
 package com.example;
 
-public class WordMapper{
-    
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) 
+            throws IOException, InterruptedException {
+        // Convert the line to string
+        String line = value.toString();
+        
+        // Split the line into words
+        String[] words = line.split("\\s+");
+        
+        // Emit each word with count 1
+        for (String str : words) {
+            if (str.length() > 0) {
+                word.set(str.toLowerCase());
+                context.write(word, one);}}}
 }
--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,9 +1,41 @@
 package com.example.controller;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import com.example.WordMapper;
+import com.example.WordReducer;
 
-public class Controller{
+public class Controller {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCount <input path> <output path>");
+            System.exit(-1);
+        }
 
-    public static void main(String[] args){
+        // Create new job
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
         
-    }
+        // Set job class
+        job.setJarByClass(Controller.class);
+        
+        // Set Mapper and Reducer classes
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+        
+        // Set output key and value classes
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        
+        // Set input and output paths
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        // Submit job and exit
+        System.exit(job.waitForCompletion(true)?0:1);}
 }
